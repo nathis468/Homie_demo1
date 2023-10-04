a8de2bedd31cf6d33f164ae0e1acd32a6e1352d5
UDHAYAKUMAR3638 <udhayakumar1220@gmail.com>
2023-10-04 13:53:20 +0530

    f1

--- demo.java
+++ demo.java
@@ -7,26 +7,14 @@ class Test{
         fun3();
     }
     static void fun1(){
-<<<<<<< HEAD
-        System.out.println("nagini nathis");
-       
-    }
-    static void fun2(){
-        String a="udhaya";
-        System.out.println(a);
-    }
-    static void fun3(){
-        String a="bala divya";
-=======
         System.out.println("nagpur nathis");
     }
     static void fun2(){
-        String a="udumbu udhay";
+        String a="udhay";
         System.out.println(a);
     }
     static void fun3(){
         String a="bard bala";
->>>>>>> 9fd8845e034a039fee4f8cd9d2fbffbbeac8590a
         System.out.println(a);
     }
 }
