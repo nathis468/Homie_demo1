a06650257fb45d01955c52f664fc2d73d6633920
nathis468 <nathis468@gmail.com>
2023-10-04 18:39:54 +0530

    commit 2

---
+++ demo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Test{
+    public static void main(String[] args){
+        fun1();
+        fun2();
+        fun3();
+    }
+    static void fun1(){
+        System.out.println("nathis");
+    }
+    static void fun2(){
+        System.out.println("udhay");
+    }
+    static void fun3(){
+        System.out.println("bala");
+    }
+}
